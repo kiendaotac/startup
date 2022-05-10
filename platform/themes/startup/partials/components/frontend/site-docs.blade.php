c5775fe530c41e26d5b81fe02c55ddef6f735ab6
Hoàng Văn Kiên <kiendaotac92@gmail.com>
2022-05-11 02:01:36 +0700

    fix style

--- platform/themes/startup/partials/components/frontend/site-docs.blade.php
+++ platform/themes/startup/partials/components/frontend/site-docs.blade.php
@@ -4,7 +4,7 @@
             <div class="bg-overlay"></div>
             <div class="col-sm-12 text-center text-white">
                 <a href="">
-                    <h2 class=" mb-0 section_header with_icon icon_color ">{{ $shortcode->title }}</h2>
+                    <h2 class=" mb-0 section_header icon_color ">{{ $shortcode->title }}</h2>
                 </a>
                 <p class="small-text text-white mb-0 col-md-10 col-md-offset-1">{{ $shortcode->subtitle }}</p>
                 <div class="text-center custom-test-tailieu col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
